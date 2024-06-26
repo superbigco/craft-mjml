superbigco/craft-mjml

commit 00db9a9048d3ca9d0c3bb024b4be7ab917541a40

chore: prepare for craft 5 (#24)
* chore: throw exception if node/mjml not found

fixes #20

Signed-off-by: Fred Carlsen <fred@sjelfull.no>

* fix: makes sure parsing will always use the site template path

fixes #21

Signed-off-by: Fred Carlsen <fred@sjelfull.no>

* chore: typos

Signed-off-by: Fred Carlsen <fred@sjelfull.no>

---------

Signed-off-by: Fred Carlsen <fred@sjelfull.no>

src/services/MJMLService.php
@@ -22,8 +22,11 @@ use mikehaertl\shellcommand\Command;
 use superbig\mjml\exceptions\MJMLException;
 use superbig\mjml\MJML;
 use superbig\mjml\models\MJMLModel;
+use Twig\Error\LoaderError;
+use Twig\Error\SyntaxError;
 use yii\base\ErrorException;
 use yii\base\Exception;
+use yii\base\InvalidConfigException;
 
 /**
  * @author    Superbig
@@ -69,10 +72,20 @@ class MJMLService extends Component
         }
     }
 
-    public function include(string $template = '', $variables = [], $renderMethod = 'cli')
+    /**
+     * @param string $template
+     * @param array $variables
+     * @param string $renderMethod
+     * @return string|null
+     * @throws LoaderError
+     * @throws SyntaxError
+     */
+    public function include(string $template = '', array $variables = [], string $renderMethod = 'cli'): ?string
     {
+        $view = Craft::$app->getView();
+
         try {
-            $templatePath = Craft::$app->getView()->resolveTemplate($template, View::TEMPLATE_MODE_SITE);
+            $templatePath = $view->resolveTemplate($template, View::TEMPLATE_MODE_SITE);
 
             if (!$templatePath) {
                 throw new MJMLException('Could not find template: ' . $template);
@@ -95,9 +108,11 @@ class MJMLService extends Component
                 throw new MJMLException('Could not render template: ' . $template);
             }
 
-            return Craft::$app->getView()->renderString($output->output(), $variables);
+            return $view->renderString($output->output(), $variables);
         } catch (MJMLException $e) {
             Craft::error('Could not generate output: ' . $e->getMessage(), 'mjml');
+
+            return null;
         }
     }
 
@@ -110,20 +125,34 @@ class MJMLService extends Component
      */
     public function parseCli(?string $html = null): ?MJMLModel
     {
+        $view = Craft::$app->getView();
+        $oldTemplateMode = $view->getTemplateMode();
+        $view->setTemplateMode(View::TEMPLATE_MODE_SITE);
+        $templatesPath = $view->getTemplatesPath();
+
         $settings = MJML::$plugin->getSettings();
         $configArgs = App::parseEnv($settings->mjmlCliConfigArgs);
-
-        $mjmlCliIncludesPath = App::parseEnv($settings->mjmlCliIncludesPath);
-        if (!empty($mjmlCliIncludesPath)) {
-            $configArgs = "{$configArgs} --config.filePath {$mjmlCliIncludesPath}";
-        }
+        $configArgs = "{$configArgs} --config.filePath {$templatesPath}";
 
         $nodePath = App::parseEnv($settings->nodePath);
         $mjmlCliPath = App::parseEnv($settings->mjmlCliPath);
         $mjmlPath = "{$nodePath} {$mjmlCliPath}";
+
         $hash = md5($html);
         $tempPath = Craft::$app->getPath()->getTempPath() . "/mjml/mjml-{$hash}.html";
         $tempOutputPath = Craft::$app->getPath()->getTempPath() . "/mjml/mjml-output-{$hash}.html";
+
+        $view->setTemplateMode($oldTemplateMode);
+
+        // Check if Node.js exists
+        if (!file_exists($nodePath)) {
+            throw new InvalidConfigException("Node.js executable not found at path: {$nodePath}");
+        }
+
+        // Check if MJML CLI exists
+        if (!file_exists($mjmlCliPath)) {
+            throw new InvalidConfigException("MJML CLI executable not found at path: {$mjmlCliPath}");
+        }
 
         try {
             if (!file_exists($tempOutputPath)) {

src/models/Settings.php
@@ -25,16 +25,8 @@ class Settings extends Model
     public string $nodePath = '';
     public string $mjmlCliPath = '';
     public string $mjmlCliConfigArgs = '';
-    public string $mjmlCliIncludesPath = '';
     public string $appId = '';
     public string $secretKey = '';
-
-    public function init(): void
-    {
-        $this->mjmlCliIncludesPath = Craft::$app->getView()->getTemplatesPath();
-
-        parent::init();
-    }
 
     public function defineBehaviors(): array
     {
@@ -45,7 +37,6 @@ class Settings extends Model
                     'nodePath',
                     'mjmlCliPath',
                     'mjmlCliConfigArgs',
-                    'mjmlCliIncludesPath',
                     'appId',
                     'secretKey',
                 ],

src/templates/settings.twig
@@ -53,7 +53,7 @@
 }}
 
 {{ forms.autosuggestField({
-    label: 'MJML CLI PAth',
+    label: 'MJML CLI Path',
     instructions: 'Enter the path to where the MJML CLI installed with npm is located, i.e. `/usr/local/bin/mjml`',
     id: 'mjmlCliPath',
     name: 'mjmlCliPath',

commit 5167cdf1ae11ab17cfa06f73508aa4014d4fbea5

Fix executable node/mjml-cli path checks
A regression in 00db9a9048d3ca9d0c3bb024b4be7ab917541a40 broke setups
that depended on PATH environment variables to execute the given node
and MJML CLI binaries (`MJML_NODE_PATH` and `MJML_CLI_PATH` environment
configurations).

Various combinations with `npx`, `nvm`, `pnpm dlx` broke.

This commit introduces a more intelligent check using `is_executable`
and all PATH environment prefixes that should guarantee that an
executable will still be found. A convenient wrapper has been
introduced via a private `MJMLService::isExecutable` method.

Fixes #26

src/services/MJMLService.php
@@ -144,13 +144,13 @@ class MJMLService extends Component
 
         $view->setTemplateMode($oldTemplateMode);
 
-        // Check if Node.js exists
-        if (!file_exists($nodePath)) {
+        // Check if Node.js exists and is executable
+        if (!$this->isExecutable($nodePath)) {
             throw new InvalidConfigException("Node.js executable not found at path: {$nodePath}");
         }
 
-        // Check if MJML CLI exists
-        if (!file_exists($mjmlCliPath)) {
+        // Check if MJML CLI exists and is executable
+        if (!$this->isExecutable($mjmlCliPath)) {
             throw new InvalidConfigException("MJML CLI executable not found at path: {$mjmlCliPath}");
         }
 
@@ -210,5 +210,44 @@ class MJMLService extends Component
         }
 
         return $shellCommand->getOutput();
+    }
+
+    /**
+     * Checks whether a give path is executable.
+     *
+     * Looks through the PATH environment variables to find
+     * the binary unless an absolute path has been supplied.
+     *
+     * Makes sure the binary is executable.
+     *
+     * Caches a positive result to prevent redundant stat calls
+     * when sending large amounts of emails in one go.
+     *
+     * @param $path The executable path
+     *
+     * @return bool
+     */
+    protected function isExecutable(string $path): bool
+    {
+        static $executables = [];
+
+        if ($executables[$path] ?? false) {
+            return true; // Cached from previous checks
+        }
+
+        if (is_executable($path)) {
+            $executables[$path] = true; // Cache for next checks
+            return true;
+        }
+
+        // Look through PATH
+        foreach (explode(PATH_SEPARATOR, getenv("PATH")) as $prefix) {
+            if (is_executable($prefix.DIRECTORY_SEPARATOR.$path)) {
+                $executables[$path] = true; // Cache for next checks
+                return true;
+            }
+        }
+
+        return false; // Not found or not executable
     }
 }